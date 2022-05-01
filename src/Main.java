ff1d18cf056e25fa3dbaba350681c6f80a797803
nikkolaka <nikolaka007@gmail.com>
2022-04-30 17:14:35 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+    MyPriorityQueue<Integer> pq = new MyPriorityQueue<>();
+    pq.insert(4);
+    pq.insert(7);
+    pq.insert(5);
+    System.out.println(pq);
+    }
+}
+
